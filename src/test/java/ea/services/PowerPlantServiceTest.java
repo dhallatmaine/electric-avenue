297e885f5f4f88e8098e89ff50d19c2686774264
Derrick Hall <vectorl33t2@gmail.com>
2018-09-30 13:11:18 -0400

    Add parametric testing

--- src/test/java/ea/services/PowerPlantServiceTest.java
+++ src/test/java/ea/services/PowerPlantServiceTest.java
@@ -3,14 +3,18 @@ package ea.services;
 import com.google.common.collect.ImmutableList;
 import ea.data.PowerPlant;
 import ea.engine.GameState;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
+@RunWith(JUnitParamsRunner.class)
 public class PowerPlantServiceTest {
 
     private GameState gameState;
@@ -24,7 +28,13 @@ public class PowerPlantServiceTest {
     }
 
     @Test
-    public void shuffleDeck() {
+    @Parameters({
+            " true  | 13 Card on top of deck ",
+            " false | 13 Card not on top of the deck "
+    })
+    public void shuffleDeck(
+            boolean thirteen,
+            String description) {
         // Given
         List<PowerPlant> plantsToShuffle = ImmutableList.of(
                 new PowerPlant().withValue(10),
@@ -33,10 +43,13 @@ public class PowerPlantServiceTest {
                 new PowerPlant().withValue(13));
 
         // Act
-        List<PowerPlant> shuffled = target.shuffleDeck(plantsToShuffle, true);
+        List<PowerPlant> shuffled = target.shuffleDeck(plantsToShuffle, thirteen);
 
         // Assert
         assertThat(shuffled.size()).isEqualTo(5);
+        if (thirteen) {
+            assertThat(shuffled.get(0).getValue()).isEqualTo(13);
+        }
     }
 
 }
