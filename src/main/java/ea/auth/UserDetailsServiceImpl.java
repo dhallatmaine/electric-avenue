886dbd1345d1da8910c4e33ec2ceb416e3cb3f1d
Derrick Hall <vectorl33t2@gmail.com>
2019-04-25 22:00:16 -0400

    Working authentication, but probably failing tests

---
+++ src/main/java/ea/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package ea.auth;
+
+import ea.user.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+  import static java.util.Collections.emptyList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private UserRepository userRepository;
+
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    ea.user.User user = userRepository.findByUsername(username);
+    if (user == null) {
+      throw new UsernameNotFoundException(username);
+    }
+    return new User(user.getUsername(), user.getPassword(), emptyList());
+  }
+}
